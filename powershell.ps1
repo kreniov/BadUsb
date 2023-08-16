edcc626bc31b1b1208cb8069d6fb8ff1cb4a2e25
I1YK9N <42313093+kreniov@users.noreply.github.com>
2023-08-16 22:35:27 +0700

    Update powershell.ps1

--- powershell.ps1
+++ powershell.ps1
@@ -12,10 +12,25 @@ Write-Host "Version OS: $osVersion"
 $processor = (Get-WmiObject Win32_Processor).Name
 Write-Host "Processor: $processor"
 
+# Отображение информации о видеокарте
+$videoCard = (Get-WmiObject Win32_VideoController).Name
+Write-Host "VideoCard: $videoCard"
+
 # Отображение объема оперативной памяти
 $memory = (Get-WmiObject Win32_ComputerSystem).TotalPhysicalMemory / 1GB
 Write-Host "Memory OP: $memory GB"
 
 # Отображение объема свободного места на диске C:
 $freeSpace = (Get-WmiObject Win32_LogicalDisk -Filter "DeviceID='C:'").FreeSpace / 1GB
-Write-Host "Free memory dick C: $freeSpace GB"
+Write-Host "Free memory disk C: $freeSpace GB"
+
+# Отображение сохраненных паролей Wi-Fi
+$wifiProfiles = (netsh wlan show profiles) -match "All profils users"
+$wifiProfiles | ForEach-Object {
+    $profileName = $_ -replace "    All profils     : ", ""
+    $wifiPassword = (netsh wlan show profile name="$profileName" key=clear) -match "KeY select :"
+    if ($wifiPassword) {
+        $password = $wifiPassword[0] -replace "   KeY            : ", ""
+        Write-Host "Wi-Fi name: $profileName, Pass: $password"
+    }
+}
