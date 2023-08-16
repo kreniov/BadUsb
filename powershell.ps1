61f9985e6f4bcfe7908b613133980838ef288696
I1YK9N <42313093+kreniov@users.noreply.github.com>
2023-08-16 22:31:34 +0700

    Update powershell.ps1

--- powershell.ps1
+++ powershell.ps1
@@ -2,20 +2,20 @@
 
 # Отображение имени компьютера
 $computerName = $env:COMPUTERNAME
-Write-Host "Имя компьютера: $computerName"
+Write-Host "Name PC: $computerName"
 
 # Отображение версии операционной системы
 $osVersion = (Get-WmiObject Win32_OperatingSystem).Caption
-Write-Host "Версия операционной системы: $osVersion"
+Write-Host "Version OS: $osVersion"
 
 # Отображение информации о процессоре
 $processor = (Get-WmiObject Win32_Processor).Name
-Write-Host "Процессор: $processor"
+Write-Host "Processor: $processor"
 
 # Отображение объема оперативной памяти
 $memory = (Get-WmiObject Win32_ComputerSystem).TotalPhysicalMemory / 1GB
-Write-Host "Объем оперативной памяти: $memory GB"
+Write-Host "Memory OP: $memory GB"
 
 # Отображение объема свободного места на диске C:
 $freeSpace = (Get-WmiObject Win32_LogicalDisk -Filter "DeviceID='C:'").FreeSpace / 1GB
-Write-Host "Свободное место на диске C: $freeSpace GB"
+Write-Host "Free memory dick C: $freeSpace GB"
